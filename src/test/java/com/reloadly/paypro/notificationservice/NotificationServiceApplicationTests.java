ab15ddc6ce0d827bf0c609bcd74333230c2d4e86
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2021-02-18 00:10:09 +0100

    initial commit

---
+++ src/test/java/com/reloadly/paypro/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reloadly.paypro.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
